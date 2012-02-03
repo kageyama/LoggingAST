730cefb69f85b3add78a2f37b4f599e5e7e0c1e7
Ei <kageyama@fujisan.co.jp>
2012-02-03 17:25:01 +0900

    リファクタリング

--- src/main/groovy/jp/mystera/ast/logging/LoggingASTTransformation.groovy
+++ src/main/groovy/jp/mystera/ast/logging/LoggingASTTransformation.groovy
@@ -1,22 +1,24 @@
 package jp.mystera.ast.logging
 
 import org.apache.commons.logging.LogFactory
+import org.codehaus.groovy.ast.AnnotationNode
 import org.codehaus.groovy.ast.ClassNode
 import org.codehaus.groovy.ast.FieldNode
 import org.codehaus.groovy.ast.MethodNode
-import org.codehaus.groovy.ast.Parameter
 import org.codehaus.groovy.ast.stmt.BlockStatement
 import org.codehaus.groovy.ast.stmt.ExpressionStatement
 import org.codehaus.groovy.ast.stmt.Statement
 import org.codehaus.groovy.control.CompilePhase
 import org.codehaus.groovy.transform.ASTTransformation
 import org.codehaus.groovy.transform.GroovyASTTransformation
-import org.codehaus.groovy.ast.expr.*
 import org.gcontracts.annotations.Requires
-import org.codehaus.groovy.ast.AnnotationNode
+import org.codehaus.groovy.ast.expr.*
 
 @GroovyASTTransformation(phase = CompilePhase.SEMANTIC_ANALYSIS)
 class LoggingASTTransformation implements ASTTransformation {
+
+    private static final String LOG_FIELD_NAME = 'log'
+    private static final String LOGGING_LEVEL_INFO = "info"
 
     @Requires({
         astNodes && astNodes[0] && astNodes[0] instanceof AnnotationNode \
@@ -27,12 +29,12 @@ class LoggingASTTransformation implements ASTTransformation {
         List<ClassNode> classes = sourceUnit.AST?.classes
         classes.findAll {it.getAnnotations(new ClassNode(jp.mystera.ast.logging.WithLogging))}.each { ClassNode currentClassNode ->
             currentClassNode.addField(
-                    'log',
+                    LOG_FIELD_NAME,
                     FieldNode.ACC_PRIVATE,
                     new ClassNode(Object),
                     new MethodCallExpression(
                             new ClassExpression(new ClassNode(LogFactory)),
-                            new ConstantExpression("getLog"),
+                            new ConstantExpression("get${LOG_FIELD_NAME.capitalize()}"),
                             new ArgumentListExpression(new ConstantExpression(currentClassNode.name))
                     )
             )
@@ -56,33 +58,45 @@ class LoggingASTTransformation implements ASTTransformation {
     }
 
     private Statement createLogStatement(String prefix, MethodNode method) {
-        String methodName = method.name
-        Parameter[] parameters = method.parameters
+        String verbatimText = createVerbatimText(prefix, method)
 
-        String verbatimText = prefix + methodName + '('
-        parameters.each {
-            verbatimText += '${' + it.name + '}, '
-        }
-        verbatimText = verbatimText.substring(0, verbatimText.size() - 2)
-        verbatimText += ')'
+        List<VariableExpression> variableExpressionList = createVariableExpressionList(method)
 
-        List<VariableExpression> variableExpressionList = parameters.collect {
-            new VariableExpression(it.name)
-        }
-
-        List<ConstantExpression> constantExpressionList = [new ConstantExpression(prefix + methodName + '(')]
-        (1..parameters.size() - 1).each {constantExpressionList << new ConstantExpression(', ')}
-        constantExpressionList << new ConstantExpression(')')
+        List<ConstantExpression> constantExpressionList = createConstantExpressionList(prefix, method)
 
         def result = new ExpressionStatement(
                 new MethodCallExpression(
-                        new VariableExpression("log"),
-                        new ConstantExpression("info"),
+                        new VariableExpression(LOG_FIELD_NAME),
+                        new ConstantExpression(LOGGING_LEVEL_INFO),
                         new ArgumentListExpression(
                                 new GStringExpression(verbatimText, constantExpressionList, variableExpressionList)
                         )
                 )
         )
         return result
+    }
+
+    private createConstantExpressionList(String prefix, MethodNode method) {
+        List<ConstantExpression> constantExpressionList = [new ConstantExpression(prefix + method.name + '(')]
+        (1..method.parameters.size() - 1).each {constantExpressionList << new ConstantExpression(', ')}
+        constantExpressionList << new ConstantExpression(')')
+        return constantExpressionList
+    }
+
+    private createVariableExpressionList(MethodNode method) {
+        List<VariableExpression> variableExpressionList = method.parameters.collect {
+            new VariableExpression(it.name)
+        }
+        return variableExpressionList
+    }
+
+    private createVerbatimText(String prefix, MethodNode method) {
+        String verbatimText = prefix + method.name + '('
+        method.parameters.each {
+            verbatimText += '${' + it.name + '}, '
+        }
+        verbatimText = verbatimText.substring(0, verbatimText.size() - 2)
+        verbatimText += ')'
+        return verbatimText
     }
 }
